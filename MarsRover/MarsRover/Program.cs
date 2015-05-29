0521fd141d3143302b50374b0e5cc6db85f0d482
Gabriel Butron <gabriel.butron@gmail.com>
2015-05-29 17:44:12 -0400

    MarsRover Problem
    MarsRover Problem

---
+++ MarsRover/MarsRover/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      int plateauX, plateauY;
+      int roverX, roverY;
+      char roverOr;
+      List<Rover> roverList = new List<Rover>();
+      string line = "-1";
+      while (line != "" && line != null) 
+      {
+        line = Console.ReadLine(); //Read plateau dimension
+        string[] plateau = line.Split( ' ' );
+        plateauX = Int32.Parse( plateau[0] );
+        plateauY = Int32.Parse( plateau[1] );
+        line = Console.ReadLine();
+        while (line != "" && line != null) //Read information and orientation of each rover
+        {
+          string[] roverLine = line.Split( ' ' );
+          roverX = Int32.Parse( roverLine[0] );
+          roverY = Int32.Parse( roverLine[1] );
+          roverOr = Char.Parse( roverLine[2] );
+          line = Console.ReadLine();
+          string movement = line;
+          roverList.Add( new Rover( roverX, roverY, roverOr, movement ) );
+          line = Console.ReadLine();
+        }
+
+        foreach (Rover rover in roverList) //After reading all the info, calc the new position of each rover
+        {
+          Console.WriteLine( rover.CalcPosition( plateauX, plateauY ) );
+        }
+
+        Console.WriteLine( "==========" );
+        Console.ReadLine(); //Pause
+      }
+    }
+  }
+}
---
+++ MarsRover/MarsRover/Rover.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+  class Rover
+  {
+
+    private int[] _position;
+    private int _x = 0, _y = 1;
+    private char _orientation;
+    private string _movement;
+
+    public Rover( int x, int y, char or, string movement )
+    {
+      _position = new int[2];
+      _position[_x] = x;
+      _position[_y] = y;
+      _orientation = or;
+      _movement = movement;
+    }
+
+    public string CalcPosition( int plateauX, int plateauY )
+    {
+      foreach (char dir in _movement)
+      {
+        switch (dir)
+        {
+          case 'M':
+            MoveForward();
+            break;
+          case 'L':
+            TurnLeft();
+            break;
+          case 'R':
+            TurnRight();
+            break;
+        }
+      }
+      return (_position[_x] < 0 || _position[_x] > plateauX || _position[_y] < 0 || _position[_y] > plateauY)
+        ? "Rover is outside the plateu" :
+          _position[_x].ToString() + " " + _position[_y].ToString() + " " + _orientation;
+    }
+
+    private void MoveForward()
+    {
+      switch(_orientation)
+      {
+        case 'N':
+          _position[_y]++;
+          break;
+        case 'S':
+          _position[_y]--;
+          break;
+        case 'W':
+          _position[_x]--;
+          break;
+        case 'E':
+          _position[_x]++;
+          break;
+      }
+    }
+
+    private void TurnLeft() 
+    {
+      switch (_orientation)
+      {
+        case 'N':
+          _orientation = 'W';
+          break;
+        case 'W':
+          _orientation = 'S';
+          break;
+        case 'S':
+          _orientation = 'E';
+          break;
+        case 'E':
+          _orientation = 'N';
+          break;
+      }
+    }
+
+    private void TurnRight()
+    {
+      {
+        switch (_orientation)
+        {
+          case 'N':
+            _orientation = 'E';
+            break;
+          case 'E':
+            _orientation = 'S';
+            break;
+          case 'S':
+            _orientation = 'W';
+            break;
+          case 'W':
+            _orientation = 'N';
+            break;
+        }
+      }
+    }
+  }
+}
